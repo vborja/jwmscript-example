f1d848f31538036f02783bdefba450c052572502
vborja <vic.borja(at)gmail.com>
2008-10-14 05:43:22 -0500

    Replace new lines

--- js/preplace.js
+++ js/preplace.js
@@ -1,6 +1,6 @@
 $(window).load(function() {
   jQuery.each($("pre.replace"), function() {
-    var content = this.innerHTML.replace(/&lt;/g, "<").replace(/&gt;/g, ">");
+    var content = $(this).html().replace(/&lt;/g, "<").replace(/&gt;/g, ">").replace(/\\n/, "\n");
     $(this).replaceWith(content);
   });
 });
