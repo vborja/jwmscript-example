27ee573e79f5e1280bf3a5a69a8866d2cd7a4045
vborja <vic.borja(at)gmail.com>
2008-10-14 04:47:11 -0500

    run on window.onload

--- js/preplace.js
+++ js/preplace.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(window).load(function() {
   jQuery.each($("pre.replace"), function() {
     var content = this.innerHTML.replace(/&lt;/g, "<").replace(/&gt;/g, ">");
     $(this).replaceWith(content);
